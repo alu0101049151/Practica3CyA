b42d9b316f6208e5c0b3beed342945555056262c
Basilio Gómez Navarro <alu0101049151@ull.edu.es>
2019-10-07 14:41:51 +0100

    Confirmación 1

---
+++ cadena.h
@@ -0,0 +1,21 @@
+/* *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Computabilidad y Algoritmia (CyA)
+ * Curso: 2º
+ * Práctica 3 CyA - Calculadora para Lenguajes Formales
+ * Autor: Basilio Gómez Navarro
+ * Correo: alu0101049151@ull.edu.es
+ * Fecha 6/10/2019
+ * Archivo cadena.h: Clase cadena. Representa una secuencia finita de símbolos
+ *                   de un determinado alfabeto. Este fichero contiene la 
+ *                   definición de la clase.
+ *
+ * Referencias:
+ *                   Enunciado de la prácita:
+ *                   https://campusvirtual.ull.es/1920/pluginfile.php/159482/mod_assign/introattachment/0/
+ *                   CYA_1920_Practica_3.pdf?forcedownload=1
+ * Historial de revisiones
+ *                   6/10/2019 - Creación (primera versión) del código
+ * * /
